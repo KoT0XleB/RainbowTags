70ae5464a22e7e2d1663443cf7e9c9669b559e80
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2022-02-02 01:32:03 +0500

    Add files via upload

---
+++ RainbowTags/Config.cs
@@ -0,0 +1,46 @@
+﻿using Qurre.API.Addons;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RainbowTags
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "RainbowTags";
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+
+        [Description("Only admins and donaters can change the color of the tag?")]
+        public bool AdminOnlyEnable { get; set; } = true;
+        [Description("Color change delay. Default: 0.5")]
+        public float TimeInterval { get; set; } = 0.5f;
+        [Description("Colors for tags. You can change it any way you want.")]
+        public List<string> Color { get; set; } = new List<string>()
+        {
+            "pink",
+            "red",
+            "white",
+            //"brown",
+            "silver",
+            "light_green",
+            "crimson",
+            "cyan",
+            "aqua",
+            "deep_pink",
+            "tomato",
+            "yellow",
+            "magenta",
+            "blue_green",
+            "orange",
+            "lime",
+            //"green",
+            "emerald",
+            //"carmine",
+            //"nickel",
+            "mint",
+            //"army_green",
+            //"pumpkin"
+        };
+    }
+}
---
+++ RainbowTags/RainbowTags.cs
@@ -0,0 +1,65 @@
+﻿using Qurre;
+using Qurre.API;
+using Qurre.Events;
+using Qurre.API.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MEC;
+using Round = Qurre.API.Round;
+using Player = Qurre.API.Player;
+
+namespace RainbowTags
+{
+    public class RainbowTags : Plugin
+    {
+        public override string Developer => "KoT0XleB#4663";
+        public override string Name => "RainbowTags";
+        public override int Priority => int.MinValue;
+        public override Version Version => new Version(1, 0, 0);
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public Config CustomConfig { get; set; }
+        public void RegisterEvents()
+        {
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Round.Start += OnRoundStarted;
+
+        }
+        public void UnregisterEvents()
+        {
+            CustomConfigs.Remove(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Round.Start -= OnRoundStarted;
+        }
+        public void OnRoundStarted()
+        {
+            Timing.RunCoroutine(RainbowCoroutine(), "tags");
+        }
+        public IEnumerator<float> RainbowCoroutine()
+        {
+            int count = 0;
+            while (!Round.Ended && CustomConfig.Color.Count > 0)
+            {
+                if (count == CustomConfig.Color.Count) count = 0;
+
+                foreach (Player player in Player.List)
+                {
+                    if (CustomConfig.AdminOnlyEnable)
+                    {
+                        if (player.ServerRoles) player.RoleColor = CustomConfig.Color[count];
+                    }
+                    else player.RoleColor = CustomConfig.Color[count];
+                }
+
+                count++;
+                yield return Timing.WaitForSeconds(CustomConfig.TimeInterval);
+            }
+            yield break;
+        }
+    }
+}
