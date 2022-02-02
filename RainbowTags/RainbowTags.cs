530c876a3f6de038578361ab9320c512abfacdce
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2022-02-02 21:44:18 +0500

    Update RainbowTags.cs

--- RainbowTags/RainbowTags.cs
+++ RainbowTags/RainbowTags.cs
@@ -1,4 +1,4 @@
-﻿using Qurre;
+using Qurre;
 using Qurre.API;
 using Qurre.Events;
 using Qurre.API.Events;
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using MEC;
+using UnityEngine;
+
 using Round = Qurre.API.Round;
 using Player = Qurre.API.Player;
 
@@ -16,17 +18,17 @@ namespace RainbowTags
         public override string Developer => "KoT0XleB#4663";
         public override string Name => "RainbowTags";
         public override int Priority => int.MinValue;
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(2, 0, 0);
         public override void Enable() => RegisterEvents();
         public override void Disable() => UnregisterEvents();
-        public Config CustomConfig { get; set; }
+        public static Config CustomConfig { get; set; }
         public void RegisterEvents()
         {
             CustomConfig = new Config();
             CustomConfigs.Add(CustomConfig);
             if (!CustomConfig.IsEnable) return;
 
-            Qurre.Events.Round.Start += OnRoundStarted;
+            Qurre.Events.Player.Join += OnJoin;
 
         }
         public void UnregisterEvents()
@@ -34,32 +36,56 @@ namespace RainbowTags
             CustomConfigs.Remove(CustomConfig);
             if (!CustomConfig.IsEnable) return;
 
-            Qurre.Events.Round.Start -= OnRoundStarted;
+            Qurre.Events.Player.Join -= OnJoin;
         }
-        public void OnRoundStarted()
+        public void OnJoin(JoinEvent ev)
         {
-            Timing.RunCoroutine(RainbowCoroutine(), "tags");
-        }
-        public IEnumerator<float> RainbowCoroutine()
-        {
-            int count = 0;
-            while (!Round.Ended && CustomConfig.Color.Count > 0)
+            if (CustomConfig.AdminOnlyEnable)
             {
-                if (count == CustomConfig.Color.Count) count = 0;
-
-                foreach (Player player in Player.List)
+                if (ev.Player.ServerRoles.RemoteAdmin)
                 {
-                    if (CustomConfig.AdminOnlyEnable)
-                    {
-                        if (player.ServerRoles) player.RoleColor = CustomConfig.Color[count];
-                    }
-                    else player.RoleColor = CustomConfig.Color[count];
+                    var component = ev.Player.ReferenceHub.GetComponent<RainbowController>();
+                    if (component == null) component = ev.Player.GameObject.AddComponent<RainbowController>();
                 }
-
-                count++;
-                yield return Timing.WaitForSeconds(CustomConfig.TimeInterval);
             }
-            yield break;
+            else
+            {
+                var component = ev.Player.ReferenceHub.GetComponent<RainbowController>();
+                if (component == null) component = ev.Player.GameObject.AddComponent<RainbowController>();
+            }
+        }
+        public class RainbowController : MonoBehaviour
+        {
+            private ServerRoles _roles;
+            private string _originalColor;
+
+            private int _position = 0;
+            private float _nextCycle = 0f;
+
+            public static float interval { get; set; } = CustomConfig.TimeInterval;
+
+            private void Start()
+            {
+                _roles = GetComponent<ServerRoles>();
+                _nextCycle = Time.time;
+                _originalColor = _roles.Network_myColor;
+            }
+
+            private void OnDisable()
+            {
+                _roles.Network_myColor = _originalColor;
+            }
+
+            private void Update()
+            {
+                if (Time.time < _nextCycle) return;
+                _nextCycle += interval;
+
+                _roles.Network_myColor = CustomConfig.Color[_position];
+
+                if (++_position >= CustomConfig.Color.Count)
+                    _position = 0;
+            }
         }
     }
 }
